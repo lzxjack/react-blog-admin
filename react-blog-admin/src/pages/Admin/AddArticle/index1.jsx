7c5bc0262ebef2a252221bb11db6abd0790b3d36
Jack <965555169@qq.com>
2021-07-05 20:19:33 +0800

    精简添加文章页面代码

--- react-blog-admin/src/pages/Admin/AddArticle/index1.jsx
+++ react-blog-admin/src/pages/Admin/AddArticle/index1.jsx
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Select, Popconfirm, notification } from 'antd';
 import { CarryOutOutlined, FileDoneOutlined } from '@ant-design/icons';
 import qs from 'qs';
@@ -11,68 +11,57 @@ import './github-dark.css';
 import './index.css';
 
 const AddArticle = props => {
-    // ————————————————————判断是否是编辑模式、是否是草稿————————————————————————————
-    const [isEdit, setIsEdit] = useState(false);
-    const [isDraft, setIsDraft] = useState(false);
-    const [id, setId] = useState('');
-    const [isJudged, setIsJudged] = useState(false);
-    useEffect(() => {
-        const Edit = props.location.search !== '' ? true : false;
-        if (Edit) {
-            // 如果是编辑模式，获得id，并判断是否是草稿
-            const params = qs.parse(props.location.search.slice(1));
-            const { id, isDraft } = params;
-            setId(id);
-            const Draft = isDraft ? true : false;
-            setIsDraft(Draft);
-        }
-        setIsEdit(Edit);
-        setIsJudged(true);
-    }, [props.location.search]);
-
     // —————————————————————编辑时，获取文章详情———————————————————————
+    const [articleDetail, setArticleDetail] = useState({});
     // 从数据库获取文章/草稿详情，参数：数据库名称、文章/草稿ID
-    const getDetailFromDB = dbName => {
+    const getDetailFromDB = (dbName, id) => {
         db.collection(dbName)
             .doc(id)
             .get()
             .then(res => {
                 // 从res.data中解构赋值
                 const { title, titleEng, mainContent, tags, classes } = res.data[0];
-                setTitle(title);
-                setTitleEng(titleEng);
+                const articleInfo = {
+                    title,
+                    titleEng,
+                    mainContent,
+                    tags,
+                    classes,
+                };
+                // 文章详情存储到state中
+                setArticleDetail(articleInfo);
                 // 已有的标签存储到state
                 setSelectTags(tags);
                 // 已有的分类存储到state
                 setSelectClasses(classes);
                 // 已有的正文存储到state
-                setMainContent(mainContent);
+                setText(mainContent);
             });
     };
     // 编辑时，组件挂载，自动填入文章详情
     useEffect(() => {
-        // 还没有判断完成，直接返回
-        if (!isJudged) return;
-        //若不是编辑页面，直接返回
-        if (!isEdit) return;
-        if (!isDraft) {
-            // 向数据库获取文章详情
-            getDetailFromDB('articles');
-        } else {
-            // 向数据库获取草稿详情
-            getDetailFromDB('drafts');
+        // 判断是编辑页面再继续操作
+        if (props.location.search !== '') {
+            const params = qs.parse(props.location.search.slice(1));
+            // console.log(params.isDraft ? true : false);
+            const { id, isDraft } = params;
+            if (!isDraft) {
+                // 向数据库获取文章详情
+                getDetailFromDB('articles', id);
+            } else {
+                // 向数据库获取草稿详情
+                getDetailFromDB('drafts', id);
+            }
         }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [isEdit, isJudged, isDraft]);
+    }, [props.location.search]);
     // —————————————————————编辑时，获取文章详情end———————————————————————
 
     // ———————————标题———————————————
-    const [title, setTitle] = useState('');
-    const [titleEng, setTitleEng] = useState('');
-
+    const inputTitle = useRef();
+    const inputEng = useRef();
     // ———————————标题end———————————————
 
-    // ——————————标签————————————
+    // ——————————标签end————————————
     // 已选的标签
     const [selectTags, setSelectTags] = useState([]);
     // 标签内容改变时触发的函数
@@ -91,8 +80,7 @@ const AddArticle = props => {
 
     // ————————————正文———————————
     // 编辑区文字
-    const [mainContent, setMainContent] = useState('');
-    // 配置markdown渲染
+    const [text, setText] = useState('');
     useEffect(() => {
         // 配置highlight
         hljs.configure({
@@ -109,6 +97,10 @@ const AddArticle = props => {
             breaks: true, //默认为false。 允许回车换行。该选项要求 gfm 为true。
         });
     }, []);
+    // 文本改变，更新状态，预览区更新
+    const textChange = e => {
+        setText(e.target.innerText);
+    };
     // ————————————正文end———————————
 
     // ——————————————————————两个按钮——————————————————————
@@ -124,13 +116,13 @@ const AddArticle = props => {
             );
         db.collection(dbName)
             .add({
-                title,
-                titleEng,
-                mainContent,
+                title: inputTitle.current.value,
+                titleEng: inputEng.current.value,
+                mainContent: text,
                 tags: selectTags,
                 classes: selectClasses,
                 date: new Date().getTime(),
-                url: `https://lzxjack.top/${titleEng}`,
+                url: `https://lzxjack.top/${inputEng.current.value}`,
             })
             .then(() => {
                 // 转到草稿页/文章页
@@ -145,11 +137,11 @@ const AddArticle = props => {
             });
     };
     // 从文章数据库/草稿数据库删除，参数：数据库名、文章id
-    const removeFromDB = dbName => {
+    const removeFromDB = (dbName, id) => {
         db.collection(dbName).doc(id).remove();
     };
     // 从文章数据库/草稿数据库更新，参数：数据库名、文章id
-    const updateFromDB = dbName => {
+    const updateFromDB = (dbName, id) => {
         const page = dbName === 'articles' ? '/admin/article' : '/admin/draft';
         const message = dbName === 'articles' ? '文章更新成功！' : '草稿保存成功！';
         const icon =
@@ -161,13 +153,13 @@ const AddArticle = props => {
         db.collection(dbName)
             .doc(id)
             .update({
-                title,
-                titleEng,
-                mainContent,
+                title: inputTitle.current.value,
+                titleEng: inputEng.current.value,
+                mainContent: text,
                 tags: selectTags,
                 classes: selectClasses,
                 date: new Date().getTime(),
-                url: `https://lzxjack.top/${titleEng}`,
+                url: `https://lzxjack.top/${inputEng.current.value}`,
             })
             .then(() => {
                 // 回到文章页/草稿页
@@ -183,35 +175,39 @@ const AddArticle = props => {
     };
     // 存为草稿 按钮1
     const turnDraft = () => {
-        if (!isEdit) {
+        if (props.location.search === '') {
             // 新建文章按钮进来的
             addToDB('drafts');
         } else {
             // 修改按钮进来的
+            const params = qs.parse(props.location.search.slice(1));
+            const { id, isDraft } = params;
             if (isDraft) {
                 // 草稿页的修改按钮
-                updateFromDB('drafts');
+                updateFromDB('drafts', id);
             } else {
                 // 文章页的修改按钮
-                removeFromDB('articles');
+                removeFromDB('articles', id);
                 addToDB('drafts');
             }
         }
     };
     // 发布文章 按钮2
     const turnArticle = () => {
-        if (!isEdit) {
+        if (props.location.search === '') {
             // 新建文章按钮进来的
             addToDB('articles');
         } else {
             // 修改按钮进来的
+            const params = qs.parse(props.location.search.slice(1));
+            const { id, isDraft } = params;
             if (isDraft) {
                 // 草稿页的修改按钮
-                removeFromDB('drafts');
+                removeFromDB('drafts', id);
                 addToDB('articles');
             } else {
                 // 文章页的修改按钮
-                updateFromDB('articles');
+                updateFromDB('articles', id);
             }
         }
     };
@@ -223,19 +219,15 @@ const AddArticle = props => {
             <div className="titleBox">
                 <input
                     className="inputTitle"
+                    ref={inputTitle}
                     placeholder="请输入文章标题..."
-                    value={title}
-                    onChange={e => {
-                        setTitle(e.target.value);
-                    }}
+                    defaultValue={props.location.search !== '' ? articleDetail.title : ''}
                 />
                 <input
                     className="inputEng"
+                    ref={inputEng}
                     placeholder="请输入英文标题..."
-                    value={titleEng}
-                    onChange={e => {
-                        setTitleEng(e.target.value);
-                    }}
+                    defaultValue={props.location.search !== '' ? articleDetail.titleEng : ''}
                 />
                 <Popconfirm
                     className="draftBtn"
@@ -250,12 +242,20 @@ const AddArticle = props => {
                 <Popconfirm
                     className="pubBtn"
                     placement="bottomRight"
-                    title={`确定${isEdit && !isDraft ? '更新' : '发布'}文章吗？`}
+                    title={`确定${
+                        props.location.search !== '' &&
+                        !qs.parse(props.location.search.slice(1)).isDraft
+                            ? '更新'
+                            : '发布'
+                    }文章吗？`}
                     onConfirm={turnArticle}
                     okText="Yes"
                     cancelText="No"
                 >
-                    {isEdit && !isDraft ? '更新' : '发布'}
+                    {props.location.search !== '' &&
+                    !qs.parse(props.location.search.slice(1)).isDraft
+                        ? '更新'
+                        : '发布'}
                     文章
                 </Popconfirm>
             </div>
@@ -269,8 +269,8 @@ const AddArticle = props => {
                         showSearch
                         style={{ width: '300px' }}
                         placeholder="请选择文章分类"
-                        key={selectClasses}
-                        defaultValue={selectClasses}
+                        key={props.location.search !== '' ? articleDetail.classes : ''}
+                        defaultValue={props.location.search !== '' ? articleDetail.classes : ''}
                         onChange={classChange}
                     >
                         {props.classes}
@@ -285,8 +285,8 @@ const AddArticle = props => {
                         showArrow
                         style={{ width: '600px' }}
                         placeholder="请选择文章标签"
-                        key={selectTags}
-                        defaultValue={selectTags}
+                        key={props.location.search !== '' ? articleDetail.tags : []}
+                        defaultValue={props.location.search !== '' ? articleDetail.tags : []}
                         onChange={tagsChange}
                     >
                         {props.tags}
@@ -297,18 +297,16 @@ const AddArticle = props => {
             <div className="editBox">
                 <div
                     className="inputRegion"
-                    onInput={e => {
-                        setMainContent(e.target.innerText);
-                    }}
+                    onInput={textChange}
                     contentEditable="plaintext-only"
                     suppressContentEditableWarning
                 >
-                    {mainContent}
+                    {props.location.search !== '' ? articleDetail.mainContent : ''}
                 </div>
                 <div
                     className="showRegion markdownStyle"
                     dangerouslySetInnerHTML={{
-                        __html: marked(mainContent).replace(/<pre>/g, "<pre id='hljs'>"),
+                        __html: marked(text).replace(/<pre>/g, "<pre id='hljs'>"),
                     }}
                 ></div>
             </div>
