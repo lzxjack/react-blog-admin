18294a61198cedaa23bc29173bd91983ee5832ed
Jack <965555169@qq.com>
2021-07-07 09:56:01 +0800

    添加评论模块，使用twikoo

--- react-blog-admin/src/pages/Admin/Msg/index.jsx
+++ react-blog-admin/src/pages/Admin/Msg/index.jsx
@@ -1,7 +1,32 @@
+import { useState, useEffect } from 'react';
+import Script from 'react-load-script';
+import { twikooID } from '../../../utils/constant';
 import './index.css';
 
 const Msg = () => {
-    return <>Msg</>;
+    // twikoo源文件加载完成后，加入配置的script标签
+    const [twikooLoaded, setTwikooLoaded] = useState(false);
+    useEffect(() => {
+        if (!twikooLoaded) return;
+        const twikooConfig = document.createElement('script');
+        twikooConfig.innerHTML = `twikoo.init({envId:"${twikooID}",el:"#tcomment"})`;
+        document.body.appendChild(twikooConfig);
+        return () => {
+            document.body.removeChild(twikooConfig);
+        };
+    }, [twikooLoaded]);
+
+    return (
+        <div className="MsgBox">
+            <div id="tcomment"></div>
+            <Script
+                url="https://jack-img.oss-cn-hangzhou.aliyuncs.com/js/twikoo.all.min.js"
+                onLoad={() => {
+                    setTwikooLoaded(true);
+                }}
+            />
+        </div>
+    );
 };
 
 export default Msg;
