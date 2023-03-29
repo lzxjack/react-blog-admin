afdcbd399cac095686c2c7bc5c042ee6e9c837bc
lzxjack <965555169@qq.com>
2023-03-29 20:22:46 +0800

    feat: 登录接口

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,13 +1,39 @@
+import { notification } from 'antd';
 import classNames from 'classnames';
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { avatarUrl } from '@/utils/constant';
+import { authLogin } from '@/utils/apis/authLogin';
+import { avatarUrl, visitorEmail, visitorPwd } from '@/utils/constant';
 
 import s from './index.scss';
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const login = async (Email: string, pwd: string) => {
+    const res = await authLogin(Email, pwd);
+    openLoginNoti(res);
+    res && navigate('/home');
+  };
+
+  const openLoginNoti = (state: boolean) => {
+    state
+      ? notification.success({
+          message: '登录成功',
+          description: '欢迎进入个人博客后台管理系统！',
+          placement: 'bottomLeft',
+          duration: 1.5
+        })
+      : notification.error({
+          message: '登录失败',
+          description: '用户名或密码不正确，请重新登录！',
+          placement: 'bottomLeft',
+          duration: 1.5
+        });
+  };
 
   return (
     <div className={s.LoginBox}>
@@ -36,8 +62,12 @@ const Login: React.FC = () => {
             />
           </div>
           <div className={classNames(s.item, s.btnBox)}>
-            <div className={s.btn}>游客</div>
-            <div className={s.btn}>登录</div>
+            <div className={s.btn} onClick={() => login(visitorEmail, visitorPwd)}>
+              游客
+            </div>
+            <div className={s.btn} onClick={() => login(email, password)}>
+              登录
+            </div>
           </div>
         </div>
       </div>
--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -3,3 +3,8 @@ export const env = 'react-blog-admin-8fo571wf24c87f9';
 
 // 个人头像链接
 export const avatarUrl = 'https://img.lzxjack.top/img/202203302348298.webp';
+
+// 游客帐号信息
+export const visitorEmail = '490878306@qq.com';
+export const visitorPwd = 'asd123456';
+export const visitorAvatar = 'https://img.lzxjack.top/img/20210826221910.jpg';
