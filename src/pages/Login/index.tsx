267deab18e391a25eccdf911812129db2b7dd0c2
lzxjack <965555169@qq.com>
2023-03-29 19:28:43 +0800

    feat: login页面UI

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import classNames from 'classnames';
+import React, { useState } from 'react';
+
+import { avatarUrl } from '@/utils/constant';
 
 import s from './index.scss';
 
 const Login: React.FC = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div className={s.LoginBox}>
+      <div className={s.leftBox}>个人博客后台管理系统</div>
+      <div className={s.rightBox}>
+        <div className={s.avatarBox}>
+          <img src={avatarUrl} alt='avatar' />
+        </div>
+        <div className={s.loginBox}>
+          <div className={s.item}>
+            <div className={s.key}>账号</div>
+            <input
+              type='text'
+              className={s.value}
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+          </div>
+          <div className={s.item}>
+            <div className={s.key}>密码</div>
+            <input
+              type='password'
+              className={s.value}
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+          </div>
+          <div className={classNames(s.item, s.btnBox)}>
+            <div className={s.btn}>游客</div>
+            <div className={s.btn}>登录</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,2 +1,5 @@
 // 博客的云环境ID
 export const env = 'react-blog-admin-8fo571wf24c87f9';
+
+// 个人头像链接
+export const avatarUrl = 'https://img.lzxjack.top/img/202203302348298.webp';
