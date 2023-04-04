3f559fee6349fe33389e9cc0a0789b93cfb17b74
lzxjack <965555169@qq.com>
2023-04-04 18:10:13 +0800

    feat: update Home

--- src/pages/Admin/Home/index.tsx
+++ src/pages/Admin/Home/index.tsx
@@ -1,14 +1,26 @@
 import { useTitle } from 'ahooks';
 import React from 'react';
 
+import CountCard from '@/components/CountCard';
 import { siteTitle } from '@/utils/constant';
 
+import { useCountCarts } from './config';
 import s from './index.scss';
 
 const Home: React.FC = () => {
-  useTitle(`${siteTitle}`);
+  useTitle(siteTitle);
+  const countCarts = useCountCarts();
 
-  return <>Home</>;
+  return (
+    <>
+      {/* 统计卡片区 */}
+      <div className={s.countCardContainer}>
+        {countCarts.map(({ title, DBName }) => (
+          <CountCard key={title} title={title} DBName={DBName} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Home;
