d26efba71f3e923b32298aecfa7ede1d02b0f354
lizixuanbj01 <lizixuanbj01@kanyun.com>
2025-06-25 17:02:53 +0800

    refactor: 修改相关地址

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,42 +1,42 @@
 // 当前环境
 // export const nowEnv: 'test' | 'prod' = 'test';
-export const nowEnv: 'test' | 'prod' = 'prod';
+export const nowEnv: "test" | "prod" = "prod";
 
 // 旧版链接
-export const oldVersionUrl = 'http://124.220.38.92:84';
+export const oldVersionUrl = "http://124.222.94.201:84";
 
 // 个人基本信息
-export const userAvatar = 'https://img.lzxjack.top:99/202304061855646.webp';
-export const avatarUrl = 'https://img.lzxjack.top:99/202203302348298.webp';
+export const userAvatar = "https://img.lzxjack.top/202304061855646.webp";
+export const avatarUrl = "https://img.lzxjack.top/202203302348298.webp";
 
 // 游客帐号信息
-export const visitorEmail = '490878306@qq.com';
-export const visitorPwd = 'test123123';
-export const visitorAvatar = 'https://img.lzxjack.top:99/202304061850910.webp';
+export const visitorEmail = "490878306@qq.com";
+export const visitorPwd = "test123123";
+export const visitorAvatar = "https://img.lzxjack.top/202304061850910.webp";
 
 // 站点名称
-export const AppName = '飞鸟小站';
+export const AppName = "飞鸟小站";
 
 // 博客主页url
-export const blogUrl = 'https://lzxjack.top';
+export const blogUrl = "https://lzxjack.top";
 
 // 博客后台github
-export const githubUrl = 'https://github.com/lzxjack/blog-admin';
+export const githubUrl = "https://github.com/lzxjack/blog-admin";
 
 // 页面title
-export const siteTitle = '飞鸟小站后台管理';
+export const siteTitle = "飞鸟小站后台管理";
 
 // 数据缓存时间
 export const staleTime = 180000;
 
 // 游客修改失败后的提示语句
-export const visitorText = '游客不可以修改哦~😆';
+export const visitorText = "游客不可以修改哦~😆";
 
 // 操作失败后的提示语句
-export const failText = '操作失败，请重试！';
+export const failText = "操作失败，请重试！";
 
 // notice数据id
-export const noticeId = '2d44d6c2612a2178078ff9f553561764';
+export const noticeId = "2d44d6c2612a2178078ff9f553561764";
 
 // 分页：默认每页数量
 export const defaultPageSize = 12;
